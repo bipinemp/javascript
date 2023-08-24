880f3ab87e57fecec14d648704fbcc4b28d9f955
bipinemp <bhandaribipin3.39@gmail.com>
2023-08-24 22:47:45 +0545

    changes

---
+++ basics/json.js
@@ -0,0 +1,14 @@
+// const jsonData = `{
+//   "id": 1,
+//   "name": "Bipin",
+//   "hobbies":["football","cricket"]
+// }`;
+
+// const parsedJson = JSON.parse(jsonData);
+
+// const objectData = { name: "bipin", age: 30 };
+// const tojson = JSON.stringify(objectData);
+
+// console.log(tojson);
+
+console.log(Object.is(-0, 0));
---
+++ basics/loopsIterations.js
@@ -0,0 +1,60 @@
+// For Loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// While Loop
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do while loop
+// let i = 10;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// for...in loop
+// const users = { name: "Bipin", age: 22 };
+// for (let key in users) {
+//   console.log(key, users[key]);
+// }
+
+// for...of loop
+// const users = ["bipin", "hari", "ram"];
+// for (let user of users) {
+//   console.log(user);
+// }
+
+// break / continue
+// for (let i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     break; exit out of loop when i = 3
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     continue; 3 will be skipped
+//   }
+//   console.log(i);
+// }
+
+// labeled statements
+// mainloop: for (let i = 0; i < 5; i++) {
+//   if (i === 4) {
+//     break mainloop;
+//   }
+//   console.log(i);
+// }
+
+// mainloop: for (let i = 0; i < 5; i++) {
+//   if (i === 2) {
+//     continue mainloop;
+//   }
+//   console.log(i);
+// }
