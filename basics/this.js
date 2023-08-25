412a644ffff607f7d420796a7228465cf20ee0b0
bipinemp <bhandaribipin3.39@gmail.com>
2023-08-25 20:29:30 +0545

    changes

---
+++ basics/this.js
@@ -0,0 +1,36 @@
+// let name1 = {
+//   firstname: "Bipin",
+//   lastname: "Bhandari",
+//   print: function () {
+//     console.log(this.firstname + " " + this.lastname);
+//   },
+// };
+
+// let print1 = function (age, country) {
+//   console.log(
+//     this.firstname +
+//       " " +
+//       this.lastname +
+//       " age:  " +
+//       age +
+//       " country: " +
+//       country
+//   );
+// };
+
+// name1.print();
+
+// let name2 = {
+//   firstname: "Iron",
+//   lastname: "Man",
+// };
+
+// function borrowing
+// name1.print1.call(name2, "22");
+
+// call apply bind
+// print1.call(name2, "22");
+// print1.apply(name2, ["22", "Nepal"]);
+// let Desc = print1.bind(name2, "22", "Nepal");
+// console.log(Desc);
+// Desc();
